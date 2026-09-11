53628f2551ce6dd960c3ffe0004fdf5ec6ec0abe
untitledsaint <anthony.hatm@icloud.com>
2026-09-10 22:58:55 -0400

    Altered the project a tad bit, now it looks like a hallway from the backrooms.

--- topics/hello-world/version-control-workflow/js/script.js
+++ topics/hello-world/version-control-workflow/js/script.js
@@ -26,11 +26,11 @@ function setup() {
 */
 function draw() {
     // Make the background black (specified as RGB)
-    background(0, 0, 0);
+    background(333, 111, 111);
 
     // Draw a pyramid
     // How many levels for the pyramid
-    const levels = 10;
+    const levels = 100;
     // Loop through every level (backwards)
     for (let level = levels; level > 0; level--) {
         // Draw this layer
@@ -49,7 +49,7 @@ function draw() {
         // (320, 320) with a size based on the level
         // e.g. level 1 will be a 48x48 rectangle and
         // level 10 will be a 480x480 rectangle
-        rect(320, 320, level * 48, level * 48);
+        rect(320, 320, level * 16, level * 16);
         pop();
     }
 
@@ -58,7 +58,7 @@ function draw() {
     // No line around the shape
     noStroke();
     // Make it red (RGB)
-    fill(255, 0, 0);
+    fill(0, 0, 255);
     // Draw a 100x100 circle at the mouse position
     ellipse(mouseX, mouseY, 100, 100);
     pop();
