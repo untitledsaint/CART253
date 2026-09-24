57c865062c30bafca256e1d3c4fbc2279ca966b9
untitledsaint <anthony.hatm@icloud.com>
2026-09-24 02:02:38 -0400

    Had a sudden idea change from all the colors i had put up, why not do the "NO SIGNAL" TV test screen.
    It's pretty abstract in my opinion and it doesn't stray off too much from my original thoughts of using bright saturated colors.

--- prototypes/INSTRUCTIONS/instructions-prototype-2/js/script.js
+++ prototypes/INSTRUCTIONS/instructions-prototype-2/js/script.js
@@ -13,7 +13,7 @@
  * to the full window for immersion.
 */
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(700, 600);
   background(250, 0, 127);
   noStroke();
 }
@@ -28,50 +28,58 @@ function draw() {
   drawSquare1();
   drawSquare2();
   drawSquare3();
-  drawCircle1();
-  drawCircle2();
-  drawCircle3();
+  drawSquare4();
+  drawSquare5();
+  drawSquare6();
+  drawSquare7();
+
 
 }
 
 function drawSquare1() {
   push();
-  fill(51, 255, 20);
-  rect(80, 80, 450, 450);
+  fill(255, 0, 0);
+  rect(500, 300, 100, 100);
   pop();
 }
 
 function drawSquare2() {
   push();
-  fill(255, 65, 0);
-  rect(150, 150, 300, 300);
+  fill(255, 255, 255);
+  rect(0, 300, 100, 100);
   pop();
 }
 
 function drawSquare3() {
   push();
-  fill(0, 0, 255);
-  rect(250, 250, 100, 100);
+  fill(255, 255, 0);
+  rect(100, 300, 100, 100);
   pop();
 }
 
-function drawCircle1() {
-  push();
-  fill(255, 255, 22);
-  ellipse(100, 300, 100, 100);
-  pop();
-}
-
-function drawCircle2() {
-  push();
-  fill(255, 0, 255);
-  ellipse(300, 300, 100, 100);
-  pop();
-}
-
-function drawCircle3() {
+function drawSquare4() {
   push();
   fill(0, 255, 255);
-  ellipse(500, 300, 100, 100);
+  rect(200, 300, 100, 100);
+  pop();
+}
+
+function drawSquare5() {
+  push();
+  fill(0, 255, 0);
+  rect(300, 300, 100, 100);
+  pop();
+}
+
+function drawSquare6() {
+  push();
+  fill(255, 0, 255);
+  rect(400, 300, 100, 100);
+  pop();
+}
+function drawSquare7() {
+  push();
+  fill(0, 0, 250);
+  rect(600, 300, 100, 100);
   pop();
 }
